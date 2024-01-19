82af0623e3f41dffe68e77888c97af3cfb8bdc01
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-19 11:28:42 +0530

    factory function

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-const heavyFunction = {
-    radius : 1,
-    squareSide: 4,
-    calculateAreaOfCircle: function(radius) {
-        return Math.PI * radius * radius;
-    },
-    calculateAreaOfCircle: function(side) {
-        return side * side;
-    },
-    getCurrentDate: function() {
-        const currentDate = new Date();
-        return currentDate.toDateString();
-    },
-    performCalculations: function() {
-        const circleRadius = 5;
-        const circleArea = this.calculateAreaOfCircle(circleRadius);
-        console.log(`Area of Circle: ${circleArea}`);
-
-        setTimeout(() => {
-            const squareSide = 4;
-            const squareArea = this.calculateAreaOfCircle(squareSide);
-            console.log(`Area of Square: ${squareArea}`);
-
-            setTimeout(() => {
-                const currentDate = this.getCurrentDate();
-                console.log(`Current date: ${currentDate}`);
-            }, 3000);
-        }, 3000);
-    },
+// factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
 }
 
-heavyFunction.performCalculations();
+const circle = createCircle(1);
+circle.draw();
