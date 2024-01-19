9c7f888d17ed0b43bf05c18a584d5b3a9b206e5d
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-19 11:35:58 +0530

    factory v constrtor function

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function createCircle(radius) {
 
 const circle = createCircle(1);
 circle.draw();
+
+// Constructor Fucntion
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+const another = new Circle(1);
