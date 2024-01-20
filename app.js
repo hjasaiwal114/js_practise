5f3f5058141bed559ea2ca8dcd7eb4142a61aba5
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-21 01:17:03 +0530

    advance error handeling

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-import express from "express";
+const express = require('express');
 
-const app  = express();
+const app = express();
 
-app.get('/', (req, res) => {
-    res.status(404).json({
-        javab: "hello ",
-    });
-})
+app.get("/test", async (req, res) {
+    return res.status(200).json({ success: true});
+});
 
 app.listen(4000, () => {
-    console.log(`Server is working`)
-})
+    console.log("Server listening on port 4000");
+});
