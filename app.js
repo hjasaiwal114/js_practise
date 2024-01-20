01f68ef1d940a6de67d44caa41f0f5b5df2a157e
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-21 01:51:44 +0530

    using controller

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-app.get("/test", async (req, res) {
+const getUser  = () => undefined;
+
+app.get("/test", async (req, res) => {
+    try {
+        const user  = getUser();
+        if (!user) {
+            throw new Error("User not found");
+        }
+    } catch (error) {
+        next(error);
+    }
+
+
     return res.status(200).json({ success: true});
 });
+
+app.post("/login", async (req, res) => {
+    try {
+        const user  = getUser();
+        if (!user) {
+            throw new Error("User not found");
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error.message);
+    }
+
+
+    return res.status(200).json({ success: true});
+});
+
+app.use(errorHandler)
 
 app.listen(4000, () => {
     console.log("Server listening on port 4000");
