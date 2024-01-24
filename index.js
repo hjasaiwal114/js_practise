f57f453fed7963f3d2fb486ce63c0ac20d7ed086
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-24 17:28:57 +0530

    univerity

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
-class Shape  {
-    calaculateArea() {
-        throw new Error("Method 'CalculateArea()' must be over overidden in subclasses");
+class University {
+    constructor(name) {
+      this.name = name;
+      this.departments = [];
     }
-}
-
-class Circle extends Shape {
-    constructor(radius) {
-        super();
-        this.radius = radius;
+  
+    addDepartment(department) {
+      this.departments.push(department);
+      console.log(`Department "${department}" added to ${this.name}.`);
     }
-
-    calaculateArea() {
-        return Math.PI * this.radius * this.radius;
+  
+    removeDepartment(department) {
+      const index = this.departments.indexOf(department);
+      if (index !== -1) {
+        this.departments.splice(index, 1);
+        console.log(`Department "${department}" removed from ${this.name}.`);
+      } else {
+        console.log(`Department "${department}" does not exist in ${this.name}.`);
+      }
     }
-}
-
-class Rectangle extends Shape {
-    constructor(width, height) {
-        super();
-        this.width = width;
-        this.heigth = height;
+  
+    displayDepartments() {
+      console.log(`Departments in ${this.name}:`);
+      if (this.departments.length === 0) {
+        console.log('No departments available.');
+      } else {
+        this.departments.forEach((department, index) => {
+          console.log(`${index + 1}. ${department}`);
+        });
+      }
     }
-    calaculateArea() {
-        return this.width = this.height;
-    }
-}
-
-const circle = new Circle(7);
-const circleArea = circle.calaculateArea();
-console.log(`Circle Area: ${circleArea}`);
-
-const rectangle = new Rectangle(8, 9);
-const rectangleArea = rectangle.calaculateArea();
-console.log(`Rectangle area: ${rectangleArea}`);
+  }
+  
+  // Create an instance of the University class
+  const university = new University('ABC University');
+  
+  // Perform operations on the university
+  university.displayDepartments(); // Departments in ABC University: No departments available.
+  
+  university.addDepartment('Science');
+  university.addDepartment('Mathematics');
+  university.addDepartment('Physics');
+  university.displayDepartments();
+  // Departments in ABC University:
+  // 1. Computer Science
+  // 2. Mathematics
+  // 3. Physics
+  
+  university.removeDepartment('Mathematics');
+  university.displayDepartments();
+  // Departments in ABC University:
+  // 1. Science
+  // 2. Physics
+  
+  university.removeDepartment('Chemistry');
+  // Department "Chemistry" does not exist in ABC University.
+  
+  
