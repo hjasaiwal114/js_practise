f621788eeb932564d462ea17b51e6433430e2bfc
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-24 18:07:30 +0530

    account

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-class Product {
-    constructor(id, name, price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
+class BankAccount {
+    constructor(accountNumber, accountNumber, balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = this.accountHolderName;
+        this.balance = balance;
+    }
+    deposite(amount) {
+        this.balance += amount;
+        console.log(`Ammount $${amount} deposite into account ${this.accountNumber}.`);
     }
 
-    calculateTotalPrice(quantity) {
-        return this.price * quantity;
+    withdraw(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            console.log(`Amount $${amount} withdrawn from account ${this.accountNumber}.`);
+        } else {
+            console.log(`Insufficient balance in account ${this.accountNumber}.`)
+        }
+    }
+
+    transfer(amount, recipientAccount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            recipientAccount.deposite(amount);
+            console.log(`AMount $${amount} transferred form account ${this.accountNumber} to account ${recipientAccount.accountNumber}.`);
+        } else {
+            console.log(`Insufficient balance in account ${this.accountNumber} for transfer .`)
+        }
+    }
+    displayBalance() {
+        console.log(`Account ${this.accountNumber} balance: $${this.balance}`);
     }
 }
+const account1 = new BankAccount('SB-012', 'Karishma Hedy', 2000);
+const account2 = new BankAccount('SB-019', 'Phokas Intan', 3000);
 
-class PersonalCareProduct extends Product {
-    constructor(id, name, price, warrantyPeriod) {
-        super(id, name, price);
-        this.warrantyPeriod = warrantyPeriod;
-    }
+// Transactions on the bank accounts
+account1.displayBalance(); // Account SB-012 balance: $2000
+account2.displayBalance(); // Account SB-019 balance: $3000
 
-    calculateTotalPrice(quantity) {
-        const totalPrice = super.calculateTotalPrice(quantity);
-        return totalPrice + this.warrantyPeriod;
-    }
-}
+account1.deposit(500); // Amount $500 deposited into account SB-012.
+account1.displayBalance(); // Account SB-012 balance: $2500
 
-const PersonalCareProduct = new PersonalCareProduct(1, "Shampoo", 10, 2);
-const totalPrice = PersonalCareProduct.calculateTotalPrice(3);
+account1.withdraw(200); // Amount $200 withdrawn from account SB-012.
+account1.displayBalance(); // Account SB-012 balance: $2300
 
-console.log(`Total Price: $${totalPrice}`);
+account1.transfer(700, account2); // Amount $700 transferred from account SB-01 to account SB-019.
+account1.displayBalance(); // Account SB-012 balance: $1600
+account2.displayBalance();
