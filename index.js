2905ad66778b9751ff757c54e4e3fdad15654f40
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-24 15:05:24 +0530

    cretaing instance

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+// Write a JavaScript program to create a class called "Person" with properties for name, age and country. Include a method to display the person's details. Create two instances of the 'Person' class and display their details.
+
+class Person {
+    constructor(name, age, country) {
+        this.name  = name;
+        this.age  =  age;
+        this.country = country;
+    }
+
+    displayDetails() {
+        console.log(`Nmae: ${this.name}`);
+        console.log(`Age : ${this.age}`);
+        console.log(`Country: ${this.country}`);
+    }
+}
+
+// create instance of the person class
+const person1  = new Person('Francisca Rohan', 25, 'USA');
+const person2 = new Person('Raimond Aruna', 30, 'Netherland');
+
+console.log('Perosn-1 Details:');
+person1.displayDetails();
+
+console.log('\nPerson-2 Details:');
+person2.displayDetails()
