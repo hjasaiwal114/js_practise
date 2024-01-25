feed5d093badfcbf91885bd439ac0178c31e5356
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-26 00:58:15 +0530

    doubly linked list

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 class Node {
-	constructor(value) {
-		this.value = value;
-		this.next = null;
-		this.previous = null;
-	}
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
 }
 
-class DoublyLinkedList {
-	constructor(value) {
-		this.head = {
-			value: value,
-			next: null,
-			previous: null
-		};
-		this.length = 0;
-		this.tail = this.head;
-	}
+class DoublyLinkedLidt {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            previous: null
+        };
+        this.length = 0;
+        this.tail = this.head;
+    }
 
-	// Insert node at end of the list
-	add(newNode) {
-		this.tail.next = newNode;
-		newNode.previous = this.tail;
-		this.tail = newNode;
-		this.length++;
-	}
+    add(newNode) {
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else 
+        {
+            newNode.previous = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length ++;
+    }
+
+    size() {
+        return this.length;
+    }
 
     printList() {
-		let current = this.head;
-		let result = [];
-		while (current !== null) {
-			result.push(current.value);
-			current = current.next;
-		}
-		console.log(result.join(' '));
-		return this;
-	}
+        let current = this.head;
+        let result = [];
+        while (current !== null) {
+            result.push(current.value);
+            current = current.next;
+        }
+        console.log(result.json(' '));
+        return this;
+    }
 }
+
 
 let numList = new DoublyLinkedList();
 numList.add(new Node(2));
 numList.add(new Node(3));
-numList.add(new Node(4));
 numList.add(new Node(5));
 numList.add(new Node(6));
-numList.add(new Node(7));
+numList.add(new Node(8));
+console.log("Original Doubly Linked Lists:")
 numList.printList();
+let linkedlist_size = numList.size();
+console.log("Size of the said Doubly Linked lists: "+linkedlist_size);
