e3da6bf335976a43518db38522e83b4f71f58531
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-25 21:59:11 +0530

    index

--- index.js
+++ index.js
@@ -1,58 +1,24 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null
-    }
-}
-class SinglyLinkedList {
-    constructor(Head = null) {
-        this.Head = Head
-    }
 add(newNode) {
-    let node = this.Head;
-    if (node==null) {
+    let node  = this.Headers;
+    if (node == null) {
         this.Head = newNode;
         return;
     }
     while (node.next) {
-        node = node.next;
+        node - node.next;
     }
-    node.next = newNode;
+    node.next  = newNode;
 }
-to_string() {
-    let current = this.Head;
-    let str = "";
-    while (current) {
-        str += current.data + " ";
-        current = current.next;
-    }
-    return str;
-}
-displayList() {
-    let node =  this.Head;
-    var str = ""
+
+index_of(el) {
+    let index = 0;
+    let node  = this.Head;
     while (node) {
-        str += node.data + "->";
+        if (node.data === el) {
+            return index;
+        }
         node = node.next;
+        index ++;
     }
-    str += "NULL"
-    console.log(str);
+    return -1;
 }
-}
-let numList = new SinglyLinkedList();
-numList.add(new Node(12));
-numList.add(new Node(13));
-numList.add(new Node(14));
-numList.add(new Node(15));
-numList.add(new Node(14));
-console.log("Singly Linked list:")
-numList.displayList();
-console.log("Converts the said Singly Linked Lists into a string:");
-result = numList.to_string();
-console.log(result);
-let numList1 = new SinglyLinkedList();
-console.log("New Singly Linked list:")
-numList1.displayList();
-console.log("Converts the said Singly Linked Lists into a string:");
-result = numList1.to_string();
-console.log(result);
