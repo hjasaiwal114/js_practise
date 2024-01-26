e776c81ed1b876bf9e2a6433fe55bc5d606a1beb
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-26 23:40:25 +0530

    done

--- index.js
+++ index.js
@@ -1,59 +1,55 @@
 class Node {
-	constructor(value) {
-		this.value = value;
-		this.next = null;
-		this.previous = null;
-	}
-}
-
-class DoublyLinkedList {
-	constructor(value) {
-		this.head = {
-			value: value,
-			next: null,
-			previous: null
-		};
-		this.length = 0;
-		this.tail = this.head;
-	}
-
-  add(newNode) {
-    if (this.head === null) {
-    this.head = newNode;
-    this.tail = newNode;
-    } 
-    else
-    {
-    newNode.previous = this.tail;
-    this.tail.next = newNode;
-    this.tail = newNode;
-  }
-
-  this.length++;
-   }
-  
-is_present(val) {
-    let current = this.head;
-    while (current) {
-    if (current.value === val) {
-      return true;
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
     }
-    current = current.next;
-  }
-  return false;
+}
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            previous: null
+        };
+        this.length = 0;
+        this.tail = this.head;
+    }
+add(newNode) {
+    if (this.head === null) {
+        this.head = newNode;
+        this.tail = newNode;
+    }
+    else 
+    {
+        newNode.previous = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+    this.length++;
+}
+update_Node(index, newValue) {
+    if (index < 0 || index > this.length) {
+        return false;
+    }
+    let currentNode = this.head;
+    for (let i = 0; i< index; i++) {
+        currentNode=  currentNode.next;
+    }
+    currentNode.value = newValue;
+    return true;
 }
 
-  
- printList() {
-		let current = this.head;
-		let result = [];
-		while (current !== null) {
-			result.push(current.value);
-			current = current.next;
-		}
-		console.log(result.join(' '));
-		return this;
-	}
+printList() {
+    let current = this.head;
+    let result = [];
+    while (current !== null) {
+        result.push(current.value);
+        current = current.next;
+    }
+    console.log(result.join(' '));
+    return this;
+}
 }
 
 let numList = new DoublyLinkedList();
@@ -65,11 +61,12 @@ numList.add(new Node(6));
 numList.add(new Node(7));
 console.log("Original Doubly Linked Lists:")
 numList.printList();
-result = numList.is_present(2);
-console.log("Is 2 present in the said link list: "+result);
-result = numList.is_present(7);
-console.log("Is 7 present in the said link list: "+result);
-result = numList.is_present(1);
-console.log("Is 1 present in the said link list: "+result);
-result = numList.is_present(9);
-console.log("Is 9 present in the said link list: "+result);
+console.log("Update Node value: Index = 2, Value = 33");
+numList.update_Node(2,33);
+numList.printList();
+console.log("Update Node value: Index = 6, Value = 88");
+numList.update_Node(6,88);
+numList.printList();
+console.log("Update Node value: Index = 1, Value = 55");
+numList.update_Node(1,55);
+numList.printList();
