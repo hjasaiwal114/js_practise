37d6a8067133912703f6631b49eeb1d1efce1dd2
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-27 14:05:16 +0530

    email check

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-function is_creditCard(str) 
+function valid_email(str)
 {
-    regexp = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
-
-        if (regexp.test(str))
-        {
-            return true;
-        }
-        else 
-        {
-            return false;
-        }
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(mailformat.test(str))
+{  
+console.log("Valid email address!");  
+}  
+else  
+{  
+console.log("You have entered an invalid email address!");  
+}
 }
 
-console.log(is_creditCard("378282246310006"));
-
-console.log(is_creditCard("37828224630006"));
+valid_email('me-info@example.com');
