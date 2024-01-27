a9c4d0fc01fef00c9aa29646c91424b0eab190c4
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-27 13:03:47 +0530

    al object

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-function all_values(obj) {
-    var keys = _keys(obj);
-    var length = keys.length;
-    var values = Array(length);
-    for (var i = 0; i < length; i++) {
-        values[i] = obj[keys[i]];
-    }
-    return values;
-}
-
-function _keys(obj)
-{
+function allKeys(obj) {
     if (!isObject(obj)) return [];
-    if (Object.keys) return Object.keys(obj);
     var keys = [];
-    for (var key in obj) if (_.has(obj, key)) keys.push(key);
+    for (var key in obj) keys.push(key);
     return keys;
 }
 function isObject(obj)
@@ -21,4 +9,9 @@ function isObject(obj)
     var type = typeof obj;
     return type === 'function' || type === 'object' && !!obj;
 }
-console.log(all_values({red: "#FF0000", green: "#00FF00", white: "#FFFFFF"}));
+function Student(name) {
+    this.name = name;
+}
+
+Student.prototype.rollno = true;
+console.log(allKeys(new Student("Sara")));
