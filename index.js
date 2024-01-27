4fcf0ee2e57a5c9fcbe12cb7d690dcf57ac43a1d
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-27 14:00:40 +0530

    reg ex for credit card

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function upper_case(str)
+function is_creditCard(str) 
 {
-    regexp = /^[A-Z]/;
-    if (regexp.test(str))
-    {
-        console.log("String's first character is uppercase");
-    }
-    else 
-    {
-        console.log("String's first character is not uppercase");
-    }
+    regexp = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
+
+        if (regexp.test(str))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
 }
-upper_case('Abcd');
-upper_case('abcd');
+
+console.log(is_creditCard("378282246310006"));
+
+console.log(is_creditCard("37828224630006"));
