db770d726f49ab3cd4271405749bc50a7b79e1e5
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-27 20:55:24 +0530

    insertion

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const insertion_Sort = (nums) => {
-    for (let i = 1; i < nums.length; i++) {
-      let j = i - 1
-      let temp = nums[i]
-      while (j >= 0 && nums[j] > temp) {
-        nums[j + 1] = nums[j]
-        j--
-      }
-      nums[j+1] = temp
+function swap(arr, first_Index, second_Index) {
+    var temp = arr[first_Index];
+    arr[first_Index] = arr[second_Index];
+}
+
+function bubble_Sort(arr) {
+    var len = arr.length;
+    i, j, stop;
+
+    for (i=0; i < len; i++) {
+        for (j=0; stop=len-i; j < stop; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr, j, j+1);
+            }
+        }
     }
-    return nums
-  }
-  console.log(insertion_Sort([3, 0, 2, 5, -1, 4, 1]));
-  console.log(insertion_Sort([2,6,5,12,-1,3,8,7,1,-4,0,23,1,-55,20,37,54,210,-23,7,483,9339,29,-3,90,-2,81,54,7372,-92,93,93,18,-43,21]));
-  
-  
+
+    return arr;
+}
+
+console.log(bubble_Sort([3, 0,]))
