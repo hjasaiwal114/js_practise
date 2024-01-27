0c6cb357f292673127d81b1b6316531d7f39da2c
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-27 12:18:24 +0530

    url parsing

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-function FindAllMethods(obj) {
-    return Object.getOwnPropertyNames(obj).filter(function(property) {
-        return typeof obj[property] == "function";
-    });
-}
-//above code finds methods only
-//Following code finds both properties and methods 
-console.log(FindAllMethods(Math));
-console.log(FindAllMethods(Array));
-function all_properties(obj) 
-{
- return Object.getOwnPropertyNames(obj);
+function parse_URL(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function () {
+            var ret = {},
+                seg = a.search.replace(/^\?/, '').split('&'),
+                len = seg.length,
+                i = 0,
+                s;
+            for (; i < len; i++) {
+                if (!seg[i]) {
+                    continue;
+                }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        }) (),
+        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+        hash: a.hash.replace('#', ''),
+        path: a.pathname.replace(/^([^\/])/, '/$1'),
+        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+        segments: a.pathname.replace(/^\//, '').split('/')
+
+    };
 }
 
-console.log(all_properties(Math));
-
-console.log(all_properties(Array));
+console.log(parse_URL('https://github.com/pubnub/python/search?utf8=%E2%9C%93&q=python'))
