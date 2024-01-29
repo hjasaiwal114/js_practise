78c2aafedc5673ee32d75418378aedd9ddb2b46b
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-29 17:37:53 +0530

    generic

--- app.ts
+++ app.ts
@@ -1,20 +1,11 @@
-type MyType = {
-    property: string;
-};
-
-enum MyEnum {
-    enumone= 'enumOne',
-    enumTwo = 'enumTwo',
-}
-
-
-type MyTypeTwo =  {
-    property2: boolean;
-    property3: myEnum 
-} & MyType;
-
-async function myFunction(input: MyTypeTwo) : Promise<MyTypeTwo> {
+function myFunc<T>(input: T) : T {
     return input;
 }
 
-Object.values(MyEnum).map
+const result  = myFunc<{
+    name: string;
+}>({
+    name: 'Himanshu',
+});
+
+const resultTwo  = myFunc<string[]>(["Tom"]);
