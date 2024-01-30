90ebe24ad72e4061045767b3f605861784b39e95
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-30 15:54:33 +0530

    Response

--- express_Ts/src/app.ts
+++ express_Ts/src/app.ts
@@ -1,20 +1,23 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.send("Hello world");
+app.get("/", (req:Request, res: Response) => {
+    return res.json({
+        success: true,
+        name: 'Himanshu',
+    })
 });
 
-app.post("/api/data", (req, res) => {
+app.post("/api/data", (req:Request, res: Response) => {
     console.log(req.body);
 
     return res.sendStatus(200);
 });
 
-app.all("/api/all", (req, res) => {
+app.all("/api/all", (req:Request, res: Response) => {
     return res.sendStatus(200);
 })
 
