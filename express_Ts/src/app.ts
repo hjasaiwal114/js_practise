6f3e9cf25810115a4569e7b28b71dc1cce886dfc
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-30 17:00:38 +0530

    middleware

--- express_Ts/src/app.ts
+++ express_Ts/src/app.ts
@@ -1,12 +1,17 @@
-import express, {Request, Response} from 'express'
+import express, {NextFunction,Request, Response} from 'express'
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/health', (req, res) => res.sendStatus(200));
-app.get("/ab*cd", (req, res) => res.send("/ab*cd"));
-app.get(/abc/, (req, res) => res.send("abc"));
+const middleware = 
+    {{name}: {name: string}} => 
+    (req: Request, res: Response, next: NextFunction) => {
+        req.name = name;
+        next();
+    };
+
+app.use(middleware({name: "himanshu"}));
 
 app.listen(3000, () => {
     console.log(`Application listening at http://localhost:3000`)
