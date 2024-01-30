6d632c9cb157db21f75693cba587ac9f8907750e
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-30 15:45:26 +0530

    routing

--- express_Ts/src/app.ts
+++ express_Ts/src/app.ts
@@ -1,1 +1,23 @@
-console.log("Hello world");
+import express from 'express'
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.send("Hello world");
+});
+
+app.post("/api/data", (req, res) => {
+    console.log(req.body);
+
+    return res.sendStatus(200);
+});
+
+app.all("/api/all", (req, res) => {
+    return res.sendStatus(200);
+})
+
+app.listen(3000, () => {
+    console.log(`Application listening at http://localhost:3000`)
+})
