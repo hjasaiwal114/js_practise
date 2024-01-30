356c409300ffea6a9dea919d896bfe976601d3a9
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-30 16:19:02 +0530

    route path

--- express_Ts/src/app.ts
+++ express_Ts/src/app.ts
@@ -4,20 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-app
-    .route("/api/books")
-    .get((req: Request, res: Response) => {
-        return res.send("You make Get reequest");
-    })
-    .post((req: Request, res: Response) => {
-        return res.send("You make a POST request");
-    })
-    .put((req:Request, res: Response) => {
-        return res.send("You make a PUT request");
-    })
-    .all((req: Request, res: Response) => {
-        return res.send("You make an x request");
-    });
+app.get('/health', (req, res) => res.sendStatus(200));
+app.get("/ab*cd", (req, res) => res.send("/ab*cd"));
+app.get(/abc/, (req, res) => res.send("abc"));
 
 app.listen(3000, () => {
     console.log(`Application listening at http://localhost:3000`)
