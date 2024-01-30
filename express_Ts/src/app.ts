91587791c117f7c0f7208aa0b86853fda6faec2f
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-01-30 16:15:52 +0530

    routing chainign

--- express_Ts/src/app.ts
+++ express_Ts/src/app.ts
@@ -4,22 +4,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req:Request, res: Response) => {
-    return res.json({
-        success: true,
-        name: 'Himanshu',
+app
+    .route("/api/books")
+    .get((req: Request, res: Response) => {
+        return res.send("You make Get reequest");
     })
-});
-
-app.post("/api/data", (req:Request, res: Response) => {
-    console.log(req.body);
-
-    return res.sendStatus(200);
-});
-
-app.all("/api/all", (req:Request, res: Response) => {
-    return res.sendStatus(200);
-})
+    .post((req: Request, res: Response) => {
+        return res.send("You make a POST request");
+    })
+    .put((req:Request, res: Response) => {
+        return res.send("You make a PUT request");
+    })
+    .all((req: Request, res: Response) => {
+        return res.send("You make an x request");
+    });
 
 app.listen(3000, () => {
     console.log(`Application listening at http://localhost:3000`)
