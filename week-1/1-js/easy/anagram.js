6b013ee39fb3e1a0f773d5bce066e7fc2ff02b49
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-01 17:01:04 +0530

    anagram

---
+++ week-1/1-js/easy/anagram.js
@@ -0,0 +1,15 @@
+function areAnagrams(str1, str2) {
+    const ceanStr1 = str1.replace(/\s/g, '').toLowerCase();
+    const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
+
+    return cleanStr1.split('').sort().join('') === cleanStr2.split('').sort().join('');
+}
+
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (areAnagrams(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
