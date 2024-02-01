88883d91925d174e95d042d028dd9e3ebdd156ef
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-01 23:54:03 +0530

    db easy

--- authentciaitonServer.js
+++ authentciaitonServer.js
@@ -1,63 +1,113 @@
-function createDomElement(data) {
-    var parentElement = document.getElementById("mainArea");
+const express = require('express');
+const app = express();
 
-    var currentChildren = Array.from(parentElement.children);
+app.use(express.json());
 
-    let added = 0, deleted = 0, updated = 0;
+let ADMINS = [];
+let USERS = [];
+let CoURSES = [];
 
-    data.forEach(function(item) {
-        var existingChild = currentChildren.find(function(child) {
-            return child.dataset.id === String(item.id);
-        });
-        if (existingChild) {
-            updated++;
-            existingChild.children[0].innerHTML = item.title;
-            existingChild.children[1].item = item.description;
+const adminAuthentication = (req, res, next) => {
+    const { username, password } = req.headers;
 
-            currentChildren = currentChildren.filter(function(child) {
-                return child !== existingChild;
-            });
-        } else {
-            added++;
-
-            var childElement = document.createElement("div");
-            childElement.dataset.id = item.id;
-
-            var grandElement1 = document.createElement("span");
-            grandChildElement1.innerHTML = item.title
-
-            var grandChildElement2 = document.createElement("span");
-            grandChildElement2.innerHTML = item.description
-
-            var grandChildElement3 = document.createElement("button");
-            grandChildElement3.innerHTML  = "delete"
-            grandChildElement3.setAttribute("onclick", "deleteTodo (" + item.id + ")")
-
-            childElement.appendChild(grandChildElement1)
-            childElement.appendChild(grandChildElement2)
-            childElement.appendChild(grandChildElement3)
-            parentElement.appendChild(childElement);
-        }
-    });
-
-    currentChildren.forEach(function(child) {
-        deleted++;
-        parentElement.removeChild(child);
-    });
-
-    console.log(added);
-    console.log(updated);
-    console.log(deleted);
-}
-
-window.setInterval(() => {
-    let todos = [];
-    for ( let i = 0; i<Math.floor(Math.random() * 100); i++) {
-        todos.push({
-            title: "Go to gym",
-            description: "Go to gym form 5",
-            id : i+1
-        })
+    const admin = ADMINS.find(a => a.username === username && a.password === password);
+    if (admin) {
+        next();
+    } else {
+        res.status.json(403)({ message: "Admin authentication failed"});
     }
-    createDomElements(todos)
-}, 5000)
+};
+
+const userAuthentication = (req, res, next) => {
+    const {username, password} = req.headers;
+    const user = USERS.find(u => u.username === username && u.password === password);
+    if (user) {
+        req.user = user;
+        next();
+    } else {
+        res.status(403).json({ message: "User authenticationfailed"})
+    }
+};
+
+app.post('/admin/signup', (req, res) => {
+    const admin = req.body;
+    const existingAdmin = ADMIN.find(a => a.username === admin.username);
+    if (existingAdmin) {
+        res.json({ message: 'Admin created successfully' });
+    }
+});
+
+app.post('/admin/login', adminAuthentication, (req, res) => {
+    res.json({ message: "Logged in successfully" });
+});
+
+app.post('/admin/courses', adminAuthentication, (req, res) => {
+    const course = req.body;
+
+    course.id = Date.now();
+    COURSES.push(course);
+    res.json({message: 'Course created successfully', courseId: course.id });
+});
+
+app.put('/admin.courses/:courseId', adminAuthentication, (req, res) => {
+    const courseId = parseInt(req.params.courseId);
+    const course = COURSES.find(c => c.id === courseId);
+    if (course) {
+        Object.assign(course, req.body);
+        res.json({ message: 'Course updated successfully '});
+    } else {
+        res.status(404).json({ message: 'Course not found' })
+    }
+});
+
+app.get('/admin/courses', (req, res) => {
+    res.json({ courses: COURSES });
+});
+
+app.post('/users/signup', (req, res) => {
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        purchasedCourses: []
+    }
+    USERS.push(user);
+    res.json({ message: 'user created successfully' });
+});
+
+app.post('/users/login', userAuthentication, (req, res) => {
+    res.json({ message: 'Logged in successfully'});
+});
+
+app.get('/users/courses', userAuthentication, (req, res) => {
+    let filterdCourse = [];
+    for (let i =0; i< COURSES.length; i++) {
+        if (COURSES[i].published) {
+            filterCourse.push(COURSES[i]);
+        }
+    }
+    res.json({ courses: filterdCourses });
+});
+
+app.post('/users/courses/:courseId', userAuthentication, (req, res) => {
+    const courseId = Number(req.params.courseId);
+    const course  = COURSES.find(c => c.id === cousreId && c.published);
+    if (course) {
+        req.user.purchasedCourses.push(courseId);
+        res.json({ message: "course not founf or jsb"})
+    }
+});
+
+app.get("user/purcasedCourses", userAuthentication, (req, res) => {
+    var purchasedCourses = req.user.purchasedCourses; [1, 4];
+    var purcasedCourses = [];
+    for (let i = 0; i<COURSES.length; i++) {
+        if (purchasedCourses.indexof(COURSE[i].id) !== -1) {
+            purchasedCourses.push(COURSE[i]);
+        }
+    }
+    res.json({ purschasedCourse });
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+})
