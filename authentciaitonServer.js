dea4d676459b8103ab5d181c504ec75753db7658
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-01 18:52:18 +0530

    todod server

--- authentciaitonServer.js
+++ authentciaitonServer.js
@@ -1,31 +1,105 @@
-const express = require(express);
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const bodyParse = require('body-parser');
+const fs = require("fs");
+
 const app = express();
 
+app.use(bodyParser.json());
 
-app.get('/files', (req, res) => {
-    fs.readdir(path.join(__dirname, './files/'), (err, files) => {
-        if (err) {
-            return res.status(500).json({error: 'Failed to retrieve file'});
-        }
-        res.json(files);
+function findIndex(arr, id) {
+    for (let i = 0; i< arr.length; i++) {
+        if (arr[i].id === id) return i;
+    }
+    return -1;
+}
+
+function removeAtIndex(arr, index) {
+    let newArray = [];
+    for (let i = 0; i < arr.lenght; i++) {
+        if ( i !== index) newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+app.get('/todos', (req, res) => {
+    fs.readFile("todos.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
     });
 });
 
-app.get('file/:filename', (req, res) => {
-    const filepath = path.join(__dirname, './files', req.params.filename);
-
-    fs.readFile(filepath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(404).send('File not found');
+app.get('/todos/:id',(req, res) => {
+    fs.readFile("todos.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const todos =JSON.parse(data);
+        const todoIndex = findIndex(todos, pareInt(req.params.id));
+        if (todoIndex === -1) {
+            res.status(404).send();
+        } else {
+            res.json(todos[todoIndex]);
         }
-        res.send(data);
+    });
+}) ;
+
+app.post('/todos', (req, res) => {
+    const newTodo = {
+        id: Math.floor(Math.random()* 1000000),
+        title: req.body.title,
+        description: req.body.description
+    };
+    fs.readFile("todos.json", "utf", (err, data) => {
+        if (err) throw err;
+        const todos  = JSON.parse(data);
+        todos.push(newTodo);
+        fs.writeFile("todos.json", JSON.stringify(todos), (err)=> {
+            if (err) throw err;
+            res.status(201).json(newTodo);
+        });
     });
 });
 
-app.all('*', (req, res) => {
-    res.status(404).send("Route not found");
+
+app.put('/todos/:id', (req, res) => {
+    fs.readFile("todos.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const todos = JSON.parse(data);
+        const todoIndex = findIndex(todos, parseInt(req.params.id))
+        if(todoIndex === -1) {
+            res.status(404).send();
+        } else {
+            const updateTodo ={
+                id: todos[todoIndex].id,
+                title: req.body.title,
+                description: req.body.description
+            };
+            todos[todoIndex] = updateTodo;
+            fs.writeFile("todo.json", JSON.stringify(todos), (err) =>{
+                if (err) throw err;
+                res.status(200).json(updatedTodo);
+            });
+        }
+    });
 });
+
+app.delete('todos/:id', (req, res) => {
+    fs.readFile("todos.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const todos = JSON.parse(data);
+        const todosIndex = findIndex(todos, parseInt(req.params.id))
+        if (todosIndex === -1) {
+            res.status(404).send();
+        } else {
+            todos = removeAtIndex(todos, todosIndex);
+            fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+                if (err) throw err;
+                res.send(200).send();
+            });
+        }
+    });
+});
+
+app.use((req, res, next) => {
+    res.status(404).send();
+})
 
 module.exports = app;
