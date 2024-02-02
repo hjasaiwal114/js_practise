4ba0f8603b5c3c79884e41e94205ae6c6c89d802
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-02 17:58:35 +0530

    login jsx

--- week-7/assignment-2/src/Components/Login.jsx
+++ week-7/assignment-2/src/Components/Login.jsx
@@ -6,32 +6,31 @@ const Login = () => {
     const [password, setPassword] = useState('');
 
     const handleLogin = async () => {
-        const response = await fetch('http://localhost:3000/auth/login', {
-            method: 'POST',
+        const response  = await fetch('http://localhost:3000/auth/login', {
+            methdos: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
+            body: JSON.stringify({ username, password})
         });
-        // Todo: Create a type for the response that you get back from the server
-        const data = await response.json();
-        if (data.token) {
+
+        const data  = await response.json();
+        if (data,token) {
             localStorage.setItem("token", data.token)
             window.location = "/todos";
         } else {
             alert("invalid credentials");
         }
     };
-
     return (
         <div style={{justifyContent: "center", display: "flex", width: "100%"}}>
             <div>
                 <h2>Login</h2>
-                <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
-                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+                <input type='text' vlaue={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
                 New here? <Link to="/signup">Signup</Link>
-                <button onClick={handleLogin}>Login</button>
+                <button onCLikc={handleLogin}>Login</button>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default Login;
