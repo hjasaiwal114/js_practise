f93a9fc07c4bb3cbf4451e03573dcc0979355271
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-04 18:09:36 +0530

    done

--- week-1/1-js/medium/palindrom.js
+++
@@ -1,9 +0,0 @@
-function isPalindrome(inputString) {
-    const lowercaseString = inputString.toLowerCase();
-
-    const cleanString = lowercaseString.replace(/[^a-z0-9]/g, '');
-
-    const reversedString = cleanString.split('').reverse().join('');
-
-    return cleanString === reversedString;
-}
