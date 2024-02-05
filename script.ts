4d4145b1de9a1a4505fc9a8d2199f2605fbdebc0
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-05 15:06:43 +0530

    odne

--- script.ts
+++ script.ts
@@ -1,1 +1,47 @@
+// class compostion
 
+class Engine {
+    horsePower: number;
+    fuelType: string;
+
+    constructor(horsepower: number, fuelType: string) {
+        this.horsePower = horsepower;
+        this.fuelType  = fuelType;
+    }
+}
+
+class Car {
+    make: string;
+    model: string;
+    year: number;
+    engine: Engine;
+
+    constructor(make: string, model: string, year: number, engine: Engine) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.engine = engine;
+    }
+
+    start () {
+        console.log(`The ${this.make} ${this.model} (Year: ${this.year}) is starting.`);
+    }
+
+    printCarDetails() {
+        console.log(`Car Details:`);
+        console.log(`Make: ${this.make}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Year: ${this.year}`);
+        console.log(`Engine Details:`);
+        console.log(`Horsepower: ${this.engine.horsepower}`);
+        console.log(`Fuel Type: ${this.engine.fuelType}`);
+      }
+    }
+
+const myEngine=  new Engine(200, "Gasoline");
+
+const myCar = new Car("Audi", "A3", 2023, myEngine);
+
+myCar.start();
+
+myCar.printCarDetails();
