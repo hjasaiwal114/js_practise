7e03a3f83a4d9e837b042a875daca787815cde76
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-05 14:41:09 +0530

    ..

--- script.ts
+++ script.ts
@@ -1,20 +1,25 @@
-// oops
 class Bus {
     // Properties
     make: string;
     model: string;
-    year: number
-// Constructor
+    year: number;
+  
+    // Constructor
     constructor(make: string, model: string, year: number) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
+      this.make = make;
+      this.model = model;
+      this.year = year;
     }
-}
-
-// Crete a bus object
-const myBus = new Bus("Volvo", "93784 B11R", 2019);
-
-console.log("Make:", myBus.make);     // Output: Make: Volvo
-console.log("Model:", myBus.model);   // Output: Model: 9400 B11Rr
-console.log("Year:", myBus.year); 
+  
+    // Method to start the bus
+    start() {
+      console.log(`The ${this.make} ${this.model} (Year: ${this.year}) is starting.`);
+    }
+  }
+  
+  // Create a Bus object
+  const myBus = new Bus("Volvo", "9400 B11R", 2019);
+  
+  // Call the start method to start the bus
+  myBus.start(); 
+  
