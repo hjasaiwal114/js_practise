3e3e6fe47431e8d5d554589409d09ee25216c8b2
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-22 21:05:52 +0530

    unique by set

--- 25_handy_javascript/snip3.js
+++ 25_handy_javascript/snip3.js
@@ -1,8 +1,11 @@
-const array = [1, 2,3,2,4,5,1,3];
-const uniqueArray =  array.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-});
+ const array = [1, 2,3,2,4,5,1,3];
+// const uniqueArray =  array.filter((value, index, self) => {
+//     return self.indexOf(value) === index;
+// });
 
-console.log(uniqueArray);
+// console.log(uniqueArray);
 
-const evenNumber  = array.filter(num => num % 2 === 0);
+// const evenNumber  = array.filter(num => num % 2 === 0);
+
+const uniqueArray = [... new Set(array)];
+console.group(uniqueArray);
