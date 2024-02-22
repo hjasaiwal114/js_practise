9297596e8e519c3b85eecd7b14369c5f77a1525c
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-22 20:51:37 +0530

    index of substring give index

---
+++ 25_handy_javascript/snip2.js
@@ -0,0 +1,11 @@
+// const str = "Hello, world";
+// const substring = "World";
+// const  containSubstring = str.includes(substring);
+
+// console.log(containSubstring);
+
+const str= "hello, world!";
+const substring = "World";
+const containSubstring = str.indexOf(substring) !== -1;
+
+console.log(containSubstring);
