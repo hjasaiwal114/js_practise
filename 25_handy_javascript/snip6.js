461e4b2c524d1ffd2cd01aee6be7e5325e04d362
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-02-22 21:12:17 +0530

    accednign and descending

---
+++ 25_handy_javascript/snip6.js
@@ -0,0 +1,6 @@
+const array = [3, 1, 4, 2, 5];
+const asccendingOrder = array.sort((a, b) => a - b);
+const descendingOrder = array.sort((a, b) => b - a);
+
+console.log(asccendingOrder);
+console.log(descendingOrder);
