2e4d747f186e92ada7983ebd161c543fb18e609c
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-03-13 13:04:46 +0530

    user model

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -21,3 +22,8 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 })
 
+const MONGO_URL = 'mongodb+srv://hjasaiwal114:Hh11122129@@cluster0.qsgu4r2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
