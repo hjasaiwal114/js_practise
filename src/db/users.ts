ba0f90efaabad45d1fef62d55caba31bd4e08a71
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-03-16 15:26:25 +0530

    user modules

--- src/db/users.ts
+++ src/db/users.ts
@@ -11,5 +11,23 @@ interface IUser extends Document {
 };
 
 const UserSchema = new mongoose.Schema({
-    
-})
+    username: { type: String, required: true },
+    email: { type: String, required: true},
+    authentication: {
+        password: { type: String, required: true, select: false },
+        salt: { type: String, select: false },
+        sessionToken: { type: String, select: false },
+    },
+});
+
+export const UserModel: Model<IUser> = mongoose.model<IUser>('User', UserSchema);
+
+export const getUsers = () => UserModel.find().exec();
+export const getUserByEmail = (email: string) => UserModel.findOne({email}).exec();
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
+    'authentication.sessionToken': sessionToken,
+}).exec();
+export const getUserById = (id: string) => UserModel.findById(id).exec();
+export const createUser = (value: Record<string, any>) => UserModel.create(values);
+export const deleteUserById = (id: string) => UserModel.findByIdAndDelete(id).exec();
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values, {new: true}).exec();
