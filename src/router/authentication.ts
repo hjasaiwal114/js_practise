c29bff7a06526c13ff6d7bb90eb95365ccd3ba92
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-03-18 19:41:53 +0530

    authentication routers

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -4,4 +4,5 @@ import { register } from '../controllers/authentication';
 
 export default ( router: express.Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
 }
