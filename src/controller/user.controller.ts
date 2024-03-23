4bb0d0c005ab677be530eb59d507f2a6837bc845
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-03-23 18:27:39 +0530

    user controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -5,6 +5,7 @@ import logger from "../utils/logger";
 export function createUserHandler(req: Request, res: Response) {
     try{
       const user = await createUser(req.body);
+      return user;
     }
     catch(e){
         logger.error(e)
