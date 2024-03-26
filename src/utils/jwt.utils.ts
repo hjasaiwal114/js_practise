aec8e98aeeb6fc49441c5836ca2eef1ad047db45
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2024-03-26 22:58:06 +0530

    .

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,10 +1,18 @@
 import jwt from 'jsonwebtoken'
 import config from 'config'
 
-function signJwt() {
+const privateKey = config.get<string>("privateKey")
+const publicKey = config.get("publicKey")
 
+export function signJwt((
+    object: Object,
+    options?: jwt.SignOptions | undefined
+) {
+    return jwt.sign(object, privateKey, {
+        ...options,
+    })
 }
 
 function verifyJwt(){
-    
+
 }
